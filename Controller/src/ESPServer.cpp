a6f65cc58d916678ad263e8766219c73dbb96d1e
Stromsborg <sophus@stromsborg.dk>
2023-06-18 13:56:24 +0200

    updated the page with more features

--- Controller/src/ESPServer.cpp
+++ Controller/src/ESPServer.cpp
@@ -18,28 +18,35 @@ void ESPServer::begin(char* streamServer, char* snapshotServer) {               
   
         String streamServerString = streamServer;
         String snapshotServerString = snapshotServer;
+        String adminServerString = "http://192.168.1.80";
         String snapshotPath = "/snapshot";
 
         String html = "<!DOCTYPE html>";
         html += "<html>";
         html += "<head>";
-        html += "<title>ESP32 Stream Server</title>";
+        html += "<title>My Lock Web Server</title>";
         html += "<style>";
-        html += "body { text-align: center; padding: 20px; background-color: #282c34; color: #61dafb; font-family: Arial, sans-serif; }";
-        html += "h1 { font-size: 2em; }";
-        html += "img { border-radius: 15px; }";
-        html += "button { padding: 10px 20px; font-size: 1em; border-radius: 5px; color: #282c34; background-color: #61dafb; border: none; cursor: pointer; transition: background-color 0.3s; }";
-        html += "button:hover { background-color: #bbe1fa; }";
+        html += "body { font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #333; color: #fff; }";
+        html += ".blue-button { padding: 10px 20px; font-size: 16px; border-radius: 5px; background-color: #007BFF; color: #fff; border: none; margin-top: 30px; }";
+        html += ".red-button { padding: 10px 20px; font-size: 16px; border-radius: 5px; background-color: #FF4136; color: #fff; border: none; margin-top: 30px; }"; // Red button
+        html += ".loader { border: 16px solid #f3f3f3; border-top: 16px solid #3498db; border-radius: 50%; width: 70px; height: 70px; animation: spin 2s linear infinite; margin: 30px auto; }"; // Smaller loader with more margin
+        html += "@keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } }";
         html += "</style>";
         html += "</head>";
         html += "<body>";
         html += "<h1>ESP32 Stream</h1>";
-        html += "<img id='stream' src='" + streamServerString + "' alt='Stream not available' style='width:80%; max-width:600px; margin: 20px auto; display: block;'>";
-        html += "<button id='snapshot'>Download Snapshot</button>";
-        html += "<p>Click the above button to capture a snapshot from the stream.</p>";
+        html += "<div id='loader' class='loader'></div>"; // Add loader div
+        html += "<img id='stream' src='" + streamServerString + "' onload='document.getElementById(\"loader\").style.display=\"none\"; this.style.display=\"block\";' alt='Stream not available' style='width:80%; max-width:600px; margin: 20px auto; display: none;'>";
+        html += "<button class='blue-button' id='snapshot'>Download Snapshot</button>";
+        html += "<p>Click the above button to download a snapshot.</p>";
+        html += "<button class='red-button' id='admin'>Admin Page</button>"; // New 'Admin Page' button
+        html += "<p>Click the above button to navigate to the admin page.</p>";
         html += "<script>";
         html += "document.getElementById('snapshot').onclick = function () {";
         html += "window.location.href = '" + snapshotPath + "';";
+        html += "}";
+        html += "document.getElementById('admin').onclick = function () {"; // New event for 'Admin Page' button
+        html += "window.location.href = '" + adminServerString + "';";
         html += "}";
         html += "</script>";
         html += "</body>";
@@ -51,18 +58,18 @@ void ESPServer::begin(char* streamServer, char* snapshotServer) {               
         snapshotPage += "<head>";
         snapshotPage += "<title>Snapshot</title>";
         snapshotPage += "<style>";
-        snapshotPage += "body { text-align: center; padding: 20px; background-color: #282c34; color: #61dafb; font-family: Arial, sans-serif; }";
-        snapshotPage += "h1 { font-size: 2em; }";
-        snapshotPage += "img { border-radius: 15px; }";
-        snapshotPage += "button { padding: 10px 20px; font-size: 1em; border-radius: 5px; color: #282c34; background-color: #61dafb; border: none; cursor: pointer; transition: background-color 0.3s; }";
-        snapshotPage += "button:hover { background-color: #bbe1fa; }";
+        snapshotPage += "body { font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #333; color: #fff; }";
+        snapshotPage += ".blue-button { padding: 10px 20px; font-size: 16px; border-radius: 5px; background-color: #007BFF; color: #fff; border: none; margin-top: 30px; }"; // Update button class to blue-button
+        snapshotPage += ".loader { border: 16px solid #f3f3f3; border-top: 16px solid #3498db; border-radius: 50%; width: 70px; height: 70px; animation: spin 2s linear infinite; margin: 30px auto; }"; // Smaller loader with more margin
+        snapshotPage += "@keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } }";
         snapshotPage += "</style>";
         snapshotPage += "</head>";
         snapshotPage += "<body>";
         snapshotPage += "<h1>ESP32 Snapshot</h1>";
-        snapshotPage += "<img id='snapshot' src='" + snapshotServerString + "' alt='Snapshot not available' style='width:80%; max-width:600px; margin: 20px auto; display: block;'>";
-        snapshotPage += "<button id='goBack'>Go Back</button>";
-        snapshotPage += "<p>Click the above button to go back to the live stream.</p>";
+        snapshotPage += "<div id='loader' class='loader'></div>"; // Add loader div
+        snapshotPage += "<img id='snapshot' src='" + snapshotServerString + "' onload='document.getElementById(\"loader\").style.display=\"none\"; this.style.display=\"block\";' alt='Snapshot not available' style='width:80%; max-width:600px; margin: 20px auto; display: none;'>";
+        snapshotPage += "<button class='blue-button' id='goBack'>Go Back</button>"; // Update button id to 'goBack' and class to 'blue-button'
+        snapshotPage += "<p>Click the above button to go back to the stream.</p>";
         snapshotPage += "<script>";
         snapshotPage += "document.getElementById('goBack').onclick = function () {";
         snapshotPage += "window.location.href = '/';"; // This goes back to the root URL
@@ -70,6 +77,11 @@ void ESPServer::begin(char* streamServer, char* snapshotServer) {               
         snapshotPage += "</script>";
         snapshotPage += "</body>";
         snapshotPage += "</html>";
+
+
+
+
+
 
 
 
